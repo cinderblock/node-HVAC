8feef81c26e6ee8bee8f7034834db30fb7e33a2e
Cameron Tacklind <cameron@tacklind.com>
2019-02-22 03:35:57 -0800

    Update package.json at startup

--- deploy/watch-build-run.ts
+++ deploy/watch-build-run.ts
@@ -107,6 +107,7 @@ export default async function watchBuildTransferRun(options: Options) {
   };
 
   ts.createWatchProgram(host);
+  updatePackageJson();
 
   async function remoteExecNode() {
     try {
