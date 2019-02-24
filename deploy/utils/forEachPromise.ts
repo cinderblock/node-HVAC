098dc1c7b744cea7f921b4163aa433b8b54c9d31
Cameron Tacklind <cameron@tacklind.com>
2019-02-24 06:42:31 -0800

    Improve type

--- deploy/utils/forEachPromise.ts
+++ deploy/utils/forEachPromise.ts
@@ -5,6 +5,6 @@
  * @param items An array of items.
  * @param fn A function that accepts an item from the array and returns a promise.
  */
-export default function forEachPromise<T, Y>(items: T[], fn: (v: T) => Promise<Y>): Promise<Y[]> {
+export default function forEachPromise<T, Y>(items: T[], fn: (v: T) => PromiseLike<Y>): Promise<Y[]> {
   return items.reduce((promise, item) => promise.then(r => [...r, fn(item)]), Promise.resolve([]));
 }
