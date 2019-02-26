cfb87f2a012b83218d6c9a51a0b41752d3d360b7
Cameron Tacklind <cameron@tacklind.com>
2019-02-26 02:06:50 -0800

    New fancy function and expose functions differently

--- deploy/utils/debug.ts
+++ deploy/utils/debug.ts
@@ -1,13 +1,29 @@
 // Allow printing with color!
 import chalk from 'chalk';
 
-/**
- * Prints the first argument in blue, second in cyan, rest in alternating greys
- *
- * @param args List of data to print with varying colors.
- */
-function variable(...args: any[]) {
-  console.log(...args.map((val, i) => (i ? chalk.blue : i === 1 ? chalk.cyan : i % 2 ? chalk.grey : chalk.dim)(val)));
+type Colorizer = (input: string) => string;
+
+type ColorList = Colorizer[] | { colors: Colorizer[]; modulo?: number; numbers?: 'always' | 'auto' };
+
+export function makeVariableLog(colors: ColorList, prefix?: string) {
+  const arr = Array.isArray(colors) ? colors : colors.colors;
+  const mod = !Array.isArray(colors) && colors.modulo !== undefined ? colors.modulo : arr.length > 1 ? 2 : 1;
+  const numbers = !Array.isArray(colors) && colors.numbers === 'always';
+
+  function colorize(val: any, i: number): any {
+    if (!(typeof val == 'string' || (numbers && typeof val == 'number'))) return val;
+
+    if (i >= arr.length) {
+      if (!mod) return val;
+      i -= arr.length;
+      i %= mod;
+      i += arr.length - mod;
+    }
+
+    return arr[i]('' + val);
+  }
+
+  return (...args: any[]) => console.log(...(prefix ? [prefix, ...args] : args).map(colorize));
 }
 
 function makeChalkLog(color: (s: string) => string) {
@@ -16,22 +32,20 @@ function makeChalkLog(color: (s: string) => string) {
   };
 }
 
-const green = makeChalkLog(chalk.green);
-const red = makeChalkLog(chalk.red);
-const yellow = makeChalkLog(chalk.yellow);
-const magenta = makeChalkLog(chalk.magenta);
-const grey = makeChalkLog(chalk.grey);
+export const green = makeChalkLog(chalk.green);
+export const red = makeChalkLog(chalk.red);
+export const yellow = makeChalkLog(chalk.yellow);
+export const magenta = makeChalkLog(chalk.magenta);
+export const cyan = makeChalkLog(chalk.cyan);
+export const grey = makeChalkLog(chalk.grey);
 
-export default {
-  log: console.log,
-  variable,
-  green,
-  yellow,
-  red,
-  magenta,
-  grey,
-  error: red,
-  warn: yellow,
-  notice: magenta,
-  info: grey,
-};
+export const noColor = console.log;
+
+/**
+ * Prints the first argument in blue, second in cyan, rest in alternating greys
+ *
+ * @param args List of data to print with varying colors.
+ */
+export const variable = makeVariableLog([chalk.blue, chalk.cyan, chalk.grey, chalk.dim]);
+export const error = makeVariableLog([chalk.red, chalk.cyan, chalk.yellow, chalk.grey, chalk.dim], 'Error:');
+export const info = makeVariableLog([chalk.grey, chalk.yellow, chalk.cyan, chalk.grey, chalk.dim], 'Info:');
