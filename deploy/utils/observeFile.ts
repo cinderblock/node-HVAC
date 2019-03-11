7070f8d14743a1b6620567201665c59c81eca224
Cameron Tacklind <cameron@tacklind.com>
2019-03-10 23:22:16 -0700

    Check for actual file changes

--- deploy/utils/observeFile.ts
+++ deploy/utils/observeFile.ts
@@ -1,26 +1,53 @@
-import { Observable } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
 import { watch } from 'fs';
+import { EventEmitter } from 'events';
+import { promisify } from 'util';
+
+import md5file = require('md5-file');
+
+const md5Promise = promisify(md5file);
 
 export default function observeFileChange(file: string, suppressInitial = false) {
+  const changes = new EventEmitter();
+
+  let hash: string;
+
+  md5Promise(file).then(initialHash => {
+    hash = initialHash;
+
+    const watcher = watch(file);
+
+    watcher.on('change', async (eventType: 'change' | 'rename', filename: string) => {
+      if (eventType != 'change') return;
+      console.log('Change event:', eventType, filename);
+
+      md5Promise(file)
+        .then(newHash => {
+          if (newHash !== hash) {
+            hash = newHash;
+            changes.emit('change');
+          }
+        })
+        // Catch errors reading md5 of file
+        .catch();
+    });
+
+    watcher.on('error', err => {
+      // TODO: Error handling
+      console.log('Watch error');
+    });
+
+    watcher.on('close', () => {
+      // TODO: Error handling
+      console.log('Watch close');
+    });
+  });
+
   return new Observable<void>(observer => {
     if (!suppressInitial) observer.next();
 
-    // TODO: Only emit when files actually change!
+    changes.on('change', observer.next);
 
-    watch(file)
-      .on('change', async (eventType: 'change' | 'rename', filename: string) => {
-        if (eventType == 'change') observer.next();
-        console.log('Change event:', eventType, filename);
-      })
-      .on('error', err => {
-        // TODO: Error handling
-        console.log('Watch error');
-      })
-      .on('close', () => {
-        // TODO: Error handling
-        console.log('Watch close');
-      });
-
-    // TODO: return teardown logic
+    return () => changes.removeListener('change', observer.next);
   });
 }
